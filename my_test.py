003492b32cbb4124003964046dd095e97a935665
Manjusha Chava <manjushachava@gmail.com>
2020-03-22 13:54:50 -0400

    modified my_test

--- my_test.py
+++ my_test.py
@@ -1,31 +1,55 @@
 import face_alignment
 from skimage import io
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-import base64
-from flask import Flask, request, jsonify
+"""
+# Detect 2D facial landmarks in picture
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
+input = io.imread('Baby-1.jpg')
+preds = fa.get_landmarks(input)
+print(preds)
+#  Plotting in matplotlib in 2D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(preds[0][:,0], preds[0][:,1], zdir='z', s=20, c=None, depthshade=True)
+#ax.plot(preds[0][:,0], preds[0][:,1], color=None)
+plt.show()
+"""
 
-def decode(base64_string):
-    if isinstance(base64_string, bytes):
-        base64_string = base64_string.decode("utf-8")
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, face_detector='sfd', device='cpu')
 
-    imgdata = base64.b64decode(base64_string)
-    img = io.imread(imgdata, plugin='imageio')
-    return img
+#input = io.imread('/home/cmb/singularity/toothprints-pc/landmarks/face-alignment/test/assets/aflw-test.jpg')
+input = io.imread('Baby_Face.jpg')
+preds = fa.get_landmarks_from_image(input)
+preds = np.asarray(preds)
+print(preds)
 
-app = Flask(__name__)
+print("preds shape : ", preds.shape)
+print(preds[0][0][0], preds[0][0][1], preds[0][0][2] )
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    img = decode(request.args["img"])
+#  Plotting in matplotlib in 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter3D(preds[0][:,0], preds[0][:,1], preds[0][:,2], zdir='z', s=20, c=None, depthshade=True)
+#ax.plot(preds[0][:,0], preds[0][:,1], preds[0][:,2], color=None)  # For connecting points 
 
-    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, face_detector='sfd', device='cpu')
+p1 = 60
+p2 = 61
+p3 = 62
+p4 = 63
+p5 = 64
+p6 = 65
+# Marking separate point
+ax.scatter3D(preds[0][p1][0], preds[0][p1][1], preds[0][p1][2], zdir='z', s=20, c='g', depthshade=True)
+ax.scatter3D(preds[0][p2][0], preds[0][p2][1], preds[0][p2][2], zdir='z', s=20, c='r', depthshade=True)
+ax.scatter3D(preds[0][p3][0], preds[0][p3][1], preds[0][p3][2], zdir='z', s=20, c='m', depthshade=True)
+ax.scatter3D(preds[0][p4][0], preds[0][p4][1], preds[0][p4][2], zdir='z', s=20, c='y', depthshade=True)
+ax.scatter3D(preds[0][p5][0], preds[0][p5][1], preds[0][p5][2], zdir='z', s=20, c='k', depthshade=True)
+ax.scatter3D(preds[0][p6][0], preds[0][p6][1], preds[0][p6][2], zdir='z', s=20, c='b', depthshade=True)
 
-    preds = fa.get_landmarks_from_image(img)
-    preds = np.asarray(preds)
-
-    return jsonify(preds)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=8080)
+plt.xlabel('X-axis', fontdict=None, labelpad=None)
+plt.ylabel('Y-axis', fontdict=None, labelpad=None)
+#plt.zlabel('Z-axis', fontdict=None, labelpad=None)
+plt.show()
